2377b04b2af8727111276a0a12765cd57ff122ef
Riccardo Gulin <riccardo@nucleode.com>
2020-12-15 08:59:39 +0100

    Fixes stuff; adds serve

--- src/App.js
+++ src/App.js
@@ -5,31 +5,32 @@ class App extends React.Component {
   state = {
     books: [],
   }
+  componentDidMount = async () => {
+    const apiUrl = process.env.REACT_APP_API_URL
+    const resp = await fetch(`${apiUrl}/books`)
+    const jsonBooks = await resp.json()
+    console.log(jsonBooks)
+
+    this.setState({
+      books: jsonBooks,
+    })
+  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <h1>Books in the catalogue</h1>
-          {this.state.books.map((book, index) => (
-            <div key={index}>
-              <img src={book.img} style={{ width: "200px" }}></img>
-              <span>{book.title}</span>
-            </div>
-          ))}
+          {this.state.books &&
+            this.state.books.map((book, index) => (
+              <div key={index}>
+                <img src={book.img} style={{ width: "200px" }}></img>
+                <span>{book.title}</span>
+              </div>
+            ))}
         </header>
       </div>
     )
-  }
-
-  componentDidMount = async () => {
-    const apiUrl = process.env.REACT_APP_API_URL
-    const resp = await fetch(`${apiUrl}/books`)
-    const jsonBooks = await resp.json()
-
-    this.setState({
-      books: jsonBooks.data,
-    })
   }
 }
 
